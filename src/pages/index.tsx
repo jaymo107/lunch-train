40b5945dc12d41073fa879ccc360a6bc1af1d631
James Davies <jaymo107@gmail.com>
2022-07-31 21:12:15 +0100

    Remove ugly logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4 space-y-4">
-        <img src="/logo.jpg" className="w-40 aspect-auto" alt="Train" />
+        <h1 className="text-xl text-blue-400 font-extrabold border-b border-b-blue-500 pb-4">LUNCH TRAIN</h1>
         <CreateTrain addTrain={addTrain} />
         <h3 className="border-b border-gray-200 pb-5 text-xl">Timetable</h3>
         {trains.length <= 0 && <p className="p-5 text-gray-300 uppercase italic">No trains to board</p>}
