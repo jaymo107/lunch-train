9a2ec36588707d9293ed26ee2b8c219a2fb06875
James Davies <jaymo107@gmail.com>
2022-07-31 21:08:46 +0100

    Show departed trains.

--- src/components/Train.tsx
+++ src/components/Train.tsx
@@ -33,20 +33,32 @@ const Train = (props: TrainProps) => {
         });
     };
 
-    return (
-        <div className="flex flex-col w-3/6 bg-gray-50 p-4 rounded">
-            <div className="flex justify-between items-center">
-            <h3 className="text-md font-semibold text-gray-700">{props.train.destination}</h3>
+    const hasDeparted = (): Boolean => {
+        return props.train.departsAt < new Date();
+    };
 
-            <div className="flex space-x-4 items-center">
-                <span className="text-sm font-light text-gray-500">Departs in {formatDistanceToNow(props.train.departsAt)}</span>
-                <Button onClick={deleteTrain}>X</Button>
-                <Button onClick={boardTrain}>Board</Button>
+    return (
+        <div className={`flex flex-col w-3/6 p-4 rounded ${hasDeparted() ? 'bg-white' : 'bg-gray-50'}`}>
+            <div className="flex justify-between items-center">
+                <h3 className={`text-md font-semibold ${hasDeparted() ? 'text-gray-300' : 'text-gray-700'}`}>
+                    {props.train.destination}
+                </h3>
+                <div className="flex space-x-4 items-center">
+                    <span className="text-sm font-light text-gray-500">{
+                        hasDeparted() ? 'Departed' : `Departs in ${formatDistanceToNow(props.train.departsAt)}`}
+                    </span>
+                    <Button onClick={deleteTrain}>X</Button>
+                    {!hasDeparted() && <Button onClick={boardTrain}>Board</Button>}
                 </div>
             </div>
             <div className="text-xs flex space-x-3">
                 {props.train.passengers.map((passenger: Passenger): JSX.Element => { 
-                    return <PassengerComponent key={`${passenger.name}_${passenger.id}`} {...passenger} />;
+                    return (
+                        <PassengerComponent
+                            key={`${passenger.name}_${passenger.id}`}
+                            {...passenger}
+                        />
+                    );
                 })}
             </div>
         </div>
